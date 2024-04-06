d84cb78e2e34197331e4584420b356106ac50817
Fernando Javier Tejerina <101528363+fertejj@users.noreply.github.com>
2024-04-06 18:39:54 -0300

    Punto 1 resuelto: manejo de variables

---
+++ src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        // Declaración de variables enteras y asignación de valores
+        int num1 = 8;
+        int num2 = 5;
+        int num3 = 1;
+        int num4 = 20;
+        int num5 = 9;
+
+        // Calculo del promedio
+        double promedio = (num1 + num2 + num3 + num4 + num5) / 5.0;  // Uso de 5.0 para obtener un resultado de tipo double
+
+        // Mostrar el resultado por consola
+        System.out.println("El promedio es: " + promedio);
+    }
+}

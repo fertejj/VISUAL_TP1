33c84eafa16d64db1fd0662c7c53a3589dccb68d
Fernando Javier Tejerina <101528363+fertejj@users.noreply.github.com>
2024-04-17 10:31:37 -0300

    ejercicio 10 y 11

--- src/ar/edu/unju/fi/ejercicio11/Main.java
+++ src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -1,20 +1,24 @@
 package ar.edu.unju.fi.ejercicio11;
 
 public class Main {
-    public static void main(String[] args) {
-        int j = 40;
-        int i = 2;
 
-        for (; i <= 20; i++) {
-            System.out.print(j + " ");
-            j -= 37;
-        }
-
-        for (; i <= 40; i++) {
-            System.out.print(j + " ");
-            j += 5;
-        }
-
-        System.out.println(); // Imprimir nueva línea al final
-    }
+	public static void main(String[] args) {
+		
+		Boolean cambio = true;
+		int num =  0, i = 2, prod, j;
+		
+		for ( j = 40; j > 0; j--) {
+			if (cambio == true) {
+				prod = i * j;
+				num = num + prod;
+				System.out.println(num+ " ");
+				cambio = false;
+			}else {
+				num = num + j - i;
+				i++;
+				System.out.println(num + " ");
+				
+			}	
+		}
+	}
 }
